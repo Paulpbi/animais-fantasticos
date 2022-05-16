a4c161eef04a19feeda4dfe945c64d92f41156f1
Paulo Victor <paulo16xx@hotmail.com>
2022-05-16 17:03:44 -0300

    modificado: isolamento do codigo de navegacao por tabs para dentro da funcao 'initTabNav'

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
 /**
- * navegação por tabs
+ * navegação por tabs de ul e fotos dos animais no inicio da pagina
  */
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
-tabContent[0].classList.add("ativo");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  tabContent[0].classList.add("ativo");
 
-if (tabMenu.length && tabContent.length) {
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+  if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
   }
-
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(index);
-    });
-  });
 }
+initTabNav();
