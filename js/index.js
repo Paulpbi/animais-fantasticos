bc6a48999e4f8b27b942096f0f5d3a4dbfd6f287
Paulo Victor <paulo16xx@hotmail.com>
2022-05-16 18:58:34 -0300

    finalizado: criacao das funcoes para o accordion

--- js/index.js
+++ js/index.js
@@ -22,3 +22,23 @@ function initTabNav() {
   }
 }
 initTabNav();
+
+/**
+ * accordion list
+ */
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordion();
