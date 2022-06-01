e4d22602bd235317d901810f15bab8c104d534cb
Paulo Victor <paulo16xx@hotmail.com>
2022-06-01 05:17:30 -0300

    modificado: as funcoes foram separadas em modulos, o indexJS esta importando todas as funcoes e executando

--- js/index.js
+++ js/index.js
@@ -1,90 +1,9 @@
-/**
- * navegação por tabs de ul e fotos dos animais no inicio da pagina
- */
-function initTabNav() {
-  const tabMenu = document.querySelectorAll(".js-tabmenu li");
-  const tabContent = document.querySelectorAll(".js-tabcontent section");
-  tabContent[0].classList.add("ativo");
+import initScroll from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
 
-  if (tabMenu.length && tabContent.length) {
-    function activeTab(index) {
-      tabContent.forEach((section) => {
-        section.classList.remove("ativo");
-      });
-      tabContent[index].classList.add("ativo");
-    }
-
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener("click", () => {
-        activeTab(index);
-      });
-    });
-  }
-}
-initTabNav();
-
-/**
- * accordion list
- */
-function initAccordion() {
-  const accordionList = document.querySelectorAll(".js-accordion dt");
-  if (accordionList.length) {
-    accordionList[0].classList.add("ativo");
-    accordionList[0].nextElementSibling.classList.add("ativo");
-    function activeAccordion() {
-      this.classList.toggle("ativo");
-      this.nextElementSibling.classList.toggle("ativo");
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
-    });
-  }
-}
-initAccordion();
-
-/**
- * scroll suave
- */
-function initScroll() {
-  const linksInternos = document.querySelectorAll(".js-menu a[href^='#'");
-
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  }
-
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
-}
 initScroll();
-
-/**
- * animação ao scroll
- */
-function initAnimacaoScroll() {
-  const sections = document.querySelectorAll(".js-scroll");
-
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
-
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isWindowVisible = sectionTop - windowMetade < 0;
-        if (isWindowVisible) section.classList.add("ativo");
-      });
-    }
-  }
-
-  animaScroll();
-  window.addEventListener("scroll", animaScroll);
-}
-
 initAnimacaoScroll();
+initAccordion();
+initTabNav();
