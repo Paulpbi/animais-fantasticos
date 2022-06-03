8904aa7777b8dc427acd775bb0561bf030db188c
Paulo Victor <paulo16xx@hotmail.com>
2022-06-03 10:02:58 -0300

    adicionado: module initDropDownMenu

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initDropDownMenu from "./modules/dropdown-menu.js";
 
 initScroll();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
-initTooltip()
+initTooltip();
+initDropDownMenu();
